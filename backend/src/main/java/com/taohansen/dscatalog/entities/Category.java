f8fef8bd7414fef4519bf86e7c594424963b6d03
taohansens <taohansens@gmail.com>
2022-02-27 15:57:22 -0300

    Category Resource

---
+++ backend/src/main/java/com/taohansen/dscatalog/entities/Category.java
@@ -0,0 +1,48 @@
+package com.taohansen.dscatalog.entities;
+import java.io.Serializable;
+
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        return id == category.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+}
